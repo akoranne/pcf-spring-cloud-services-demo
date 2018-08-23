d5a22851678c2eccbba2038090652a91b7a4e0a7
Ajay Koranne <akoranne@gmail.com>
2018-08-23 14:55:22 -0500

    changed to 'trial' plan, to save money

--- setup-services-pws.sh
+++ setup-services-pws.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 cf s
 cf cs cloudamqp lemur rabbit
-cf cs p-circuit-breaker-dashboard standard breaker
-cf cs p-service-registry standard registry
-cf cs p-config-server standard config -c config-server-setup.json
+cf cs p-circuit-breaker-dashboard trial breaker
+cf cs p-service-registry trial registry
+cf cs p-config-server trial config -c config-server-setup.json
+# cf cs elephantsql turtle sql
 cf s
